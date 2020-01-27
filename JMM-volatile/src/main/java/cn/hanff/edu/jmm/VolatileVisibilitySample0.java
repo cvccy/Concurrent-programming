586bd1d103ddb540312bfc2efdde934bcd15eae6
kewei <x@hanff.cn>
2020-01-27 15:53:11 +0800

    volatileVisibility

---
+++ JMM-volatile/src/main/java/cn/hanff/edu/jmm/VolatileVisibilitySample0.java
@@ -0,0 +1,39 @@
+package cn.hanff.edu.jmm;
+
+public class VolatileVisibilitySample0 {
+    private boolean initFlag = false;
+
+    public void refresh(){
+        this.initFlag = true; //普通写操作，(volatile写)
+        String threadname = Thread.currentThread().getName();
+        System.out.println("线程："+threadname+":修改共享变量initFlag");
+    }
+
+    public void load(){
+        String threadname = Thread.currentThread().getName();
+        int i = 0;
+        while (!initFlag){
+        }
+        System.out.println("线程："+threadname+"当前线程嗅探到initFlag的状态的改变");
+    }
+
+    public static void main(String[] args){
+        VolatileVisibilitySample sample = new VolatileVisibilitySample();
+        Thread threadA = new Thread(()->{
+            sample.refresh();
+        },"threadA");
+
+        Thread threadB = new Thread(()->{
+            sample.load();
+        },"threadB");
+
+        threadB.start();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        threadA.start();
+    }
+
+}
