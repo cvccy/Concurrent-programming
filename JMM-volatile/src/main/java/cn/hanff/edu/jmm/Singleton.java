e8e5a4e03e6102020ce27e90eada65f074dd16e8
kewei <x@hanff.cn>
2020-01-28 21:54:16 +0800

    volatileVisibility

---
+++ JMM-volatile/src/main/java/cn/hanff/edu/jmm/Singleton.java
@@ -0,0 +1,27 @@
+package cn.hanff.edu.jmm;
+
+public class Singleton {
+
+    /**
+     * 查看汇编指令
+     * -XX:+UnlockDiagnosticVMOptions -XX:+PrintAssembly -Xcomp
+     */
+    private volatile static Singleton myinstance;
+
+    public static Singleton getInstance() {
+        if (myinstance == null) {
+            synchronized (Singleton.class) {
+                if (myinstance == null) {
+                    myinstance = new Singleton();//对象创建过程，本质可以分文三步
+                    //对象延迟初始化
+                    //
+                }
+            }
+        }
+        return myinstance;
+    }
+
+    public static void main(String[] args) {
+        Singleton.getInstance();
+    }
+}
