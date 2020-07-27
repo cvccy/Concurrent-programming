5d3cb5f2aa82c1a1d68b5deae29bdc97c6552a04
cvccy <710723333@qq.com>
2020-07-27 11:06:53 +0800

    -----

--- JucTools/src/main/java/com/cvccy/edu/SemaphoreSample.java
+++ JucTools/src/main/java/com/cvccy/edu/SemaphoreSample.java
@@ -1,4 +1,45 @@
 package com.cvccy.edu;
 
+import java.util.concurrent.Semaphore;
+
+/**
+ * @author ：cvccy
+ * @date：2020/02/28
+ * @version: V1.0
+ * @slogan: 天下风云出我辈，一入代码岁月催
+ * @description :可用于流量控制，限制最大的并发访问数
+ */
 public class SemaphoreSample {
+
+    public static void main(String[] args) {
+        Semaphore semaphore = new Semaphore(2);
+        for (int i=0;i<5;i++){
+            new Thread(new Task(semaphore,"yangguo+"+i)).start();
+        }
+    }
+
+    static class Task extends Thread{
+        Semaphore semaphore;
+
+        public Task(Semaphore semaphore,String tname){
+            this.semaphore = semaphore;
+            this.setName(tname);
+        }
+
+        public void run() {
+            try {
+                semaphore.acquire();
+                System.out.println(Thread.currentThread().getName()+":aquire() at time:"+System.currentTimeMillis());
+
+                Thread.sleep(1000);
+
+                semaphore.release();
+                System.out.println(Thread.currentThread().getName()+":aquire() at time:"+System.currentTimeMillis());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
 }
+
